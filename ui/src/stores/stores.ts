5f97b628f082a123717992d6374c1b6571356d12
Christian <Futte8D@hotmail.com>
2023-03-17 21:11:56 +0100

    Added stores

--- ui/src/stores/stores.ts
+++ ui/src/stores/stores.ts
@@ -1,6 +1,13 @@
-import { ComponentsEnum } from './../enums/components';
 import { writable, type Writable } from 'svelte/store';
+import type { IStatusBar } from './../interfaces/IStatusBar';
+import type { UIComponentsEnum } from './../enums/UIComponentsEnum';
 
-export const component: Writable<ComponentsEnum | undefined> = writable(
-	ComponentsEnum.StatusBar
-);
+export const showComponent: Writable<UIComponentsEnum> = writable();
+
+export const statusBarStore: Writable<IStatusBar> = writable({
+	title: '',
+	description: '',
+	items: [],
+});
+
+export const showUi: Writable<boolean> = writable(true);
