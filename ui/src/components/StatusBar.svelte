a6b88cccade8dd75ea254218b0f75e218ee7a19a
Christian <Futte8D@hotmail.com>
2023-03-17 21:13:08 +0100

    Added checks

--- ui/src/components/StatusBar.svelte
+++ ui/src/components/StatusBar.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
-	import type { IStatusBarItem } from 'src/interfaces/IStatusBarItem';
+	import { statusBarStore } from './../stores/stores';
 
-	export let header: string;
-	export let description: string;
-	export let items: Array<IStatusBarItem>;
+	import { EventHandler } from './../../utils/eventHandler';
+	EventHandler();
 </script>
 
 <div class="flex min-h-screen justify-center items-end pb-5">
 	<div class="w-[300px] min-h-[100px] ps-bg-darkblue rounded-xl p-5">
 		<div class="pl-2 ps-text-lightgrey">
-			<h1 class="font-bold text-lg">{header}</h1>
-			<p class="ps-text-small mt-[-5px]">{description}</p>
+			{#if $statusBarStore.title}<h1 class="font-bold text-lg">
+					{$statusBarStore.title}
+				</h1>{/if}
+			{#if $statusBarStore.description}<p class="ps-text-small mt-[-5px]">
+					{$statusBarStore.description}
+				</p>{/if}
 
-			{#if items}
+			{#if $statusBarStore.items}
 				<div class="mt-2">
-					{#each items as item}
+					{#each $statusBarStore.items as item}
 						<div class="flex">
 							<p class="ps-text-lightgrey ps-text-small">
 								{item.key}:
