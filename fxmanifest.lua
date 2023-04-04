b421b084532cfd7f2ca052f5ab5fd444db45d4a0
Christian <Futte8D@hotmail.com>
2023-04-04 20:03:33 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,17 +5,8 @@ description 'https://github.com/Project-Sloth/ps-ui'
 
 credits 'https://github.com/sharkiller/nopixel_minigame'
 
+client_scripts {'client/*.lua'}
 
-client_scripts {
-  'client/*.lua',
-}
+ui_page {'dist/index.html'}
 
-ui_page {
-  'html/index.html'
-}
-
-files {
-    'html/js/*.js',
-    'html/index.html',
-    'html/style.css',
-}
+files {'dist/index.html', 'dist/assets/*.woff2', 'dist/assets/*.tff', 'dist/assets/*.css', 'dist/assets/*.js'}
