879099cabff05b26d99baa535c9c67363b6c14df
Christian <Futte8D@hotmail.com>
2023-04-04 18:50:42 +0200

    Small class adjustment

--- ui/src/games/MemoryGame.svelte
+++ ui/src/games/MemoryGame.svelte
@@ -376,7 +376,7 @@
 
 <div class="flex items-center justify-center min-h-screen ">
 	<div
-		class="flex items-center flex-col w-[500px]  ps-bg-darkblue p-10 shadow-lg shadow-black"
+		class="flex items-center flex-col w-[500px] ps-bg-darkblue p-10 shadow-lg shadow-black"
 	>
 		<div class="w-[20%]">
 			<Skull color={skullColor} />
