7fd0918f853f573a6d2307397b55880db99c9857
Christian <Futte8D@hotmail.com>
2023-04-04 19:53:13 +0200

    Added mock event for image

--- ui/utils/mockEvent.ts
+++ ui/utils/mockEvent.ts
@@ -3,20 +3,20 @@ export default function mockEventCall(data: unknown = {}) {
 	window.dispatchEvent(new MessageEvent('message', { data }));
 }
 
-// export function newMemoryGameMock() {
-// 	setTimeout(() => {
-// 		mockEventCall({
-// 			action: 'newMemoryGame',
-// 			data: {
-// 				game: 'MemoryGame',
-// 				amountOfAnswers: 1,
-// 				gameTime: 5,
-// 				maxAnswersIncorrect: 2,
-// 				triggerEvent: '',
-// 			},
-// 		});
-// 	}, 100);
-// }
+export function newMemoryGameMock() {
+	setTimeout(() => {
+		mockEventCall({
+			action: 'newMemoryGame',
+			data: {
+				game: 'MemoryGame',
+				amountOfAnswers: 1,
+				gameTime: 5,
+				maxAnswersIncorrect: 2,
+				triggerEvent: '',
+			},
+		});
+	}, 100);
+}
 
 export function showImageMock() {
 	setTimeout(() => {
@@ -24,7 +24,7 @@ export function showImageMock() {
 			action: 'ShowImage',
 			data: {
 				show: true,
-				url: '',
+				url: 'https://i.ytimg.com/vi/1I_AWK_BCRc/maxresdefault.jpg',
 			},
 		});
 	}, 100);
